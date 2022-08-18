2318d4fe8b1697c03a5a48777908ad4d759fd290
Diego Martín del Campo <99349485+dmdcl@users.noreply.github.com>
2022-08-18 09:46:54 -0600

    Add files via upload
    CLASS 18/08/2022

---
+++ EVA1/EVA1_1_STACK/src/eva1_1_stack/EVA1_1_STACK.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_1_stack;
+
+/**
+ *
+ * @author moviles
+ */
+public class EVA1_1_STACK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        A();
+        
+    }
+    
+    public static void A(){
+        B();
+    }
+    
+    public static void B(){
+        int i = 1;
+        double d = 10.0;
+    }
+    
+}
---
+++ EVA1/EVA1_2_STACKOVERFLOW/src/eva1_2_stackoverflow/EVA1_2_STACKOVERFLOW.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_2_stackoverflow;
+
+/**
+ *
+ * @author moviles
+ */
+public class EVA1_2_STACKOVERFLOW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        A();
+    }
+    public static void A(){
+        B();
+    }
+    
+    public static void B(){
+        A();
+    }
+    
+    
+}
