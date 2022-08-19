8bc68ca39fc13c81e3235a52af6fdb857ef0bf37
Diego Martín del Campo <99349485+dmdcl@users.noreply.github.com>
2022-08-19 09:50:53 -0600

    Add files via upload

---
+++ EVA1/EVA1_3_HEAP/src/eva1_3_heap/EVA1_3_HEAP.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_3_heap;
+
+/**
+ *
+ * @author moviles
+ */
+public class EVA1_3_HEAP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 10;
+        Prueba obj= new Prueba();
+        Prueba obj2= new Prueba();
+        System.out.println(x);
+        System.out.println(obj.valor);
+        System.out.println(obj2);
+    }
+    
+}
+
+class Prueba{
+    public int valor = 100;
+}
